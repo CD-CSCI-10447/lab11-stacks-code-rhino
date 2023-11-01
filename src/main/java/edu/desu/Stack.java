d2c1650075ff8f4debd69bb5ed1faffa0cf4efc2
Tariq Hook <thook@Tariqs-MacBook-Pro-2.local>
2023-11-01 18:54:17 -0400

    updates

--- src/main/java/edu/desu/Stack.java
+++ src/main/java/edu/desu/Stack.java
@@ -1,27 +1,50 @@
 package edu.desu;
 
+import java.util.EmptyStackException;
+
 public class Stack<T> {
     private Node top;
     private int size;
 
+    private class Node {
+        T data;
+        Node next;
+
+        Node(T data) {
+            this.data = data;
+        }
+    }
 
     public void push(T data) {
+        Node newNode = new Node(data);
+        newNode.next = top;
+        top = newNode;
+        size++;
     }
 
     public T pop() {
-        return null;
+        if (top == null) {
+            throw new EmptyStackException();
+        }
+        T data = top.data;
+        top = top.next;
+        size--;
+        return data;
     }
 
     public T peek() {
-        return null;
+        if (top == null) {
+            throw new EmptyStackException();
+        }
+        return top.data;
     }
 
-    public Boolean isEmpty() {
-        return  null;
+    public boolean isEmpty() {
+        return top == null;
     }
 
     public int size() {
-        return -1;
+        return size;
     }
 }
 
